d7a194693a768dc03d460485f4bd1d570b1bee2a
espoupou <126259211+espoupou@users.noreply.github.com>
2023-06-06 14:07:50 +0100

    Update 6-print_comb3.py

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 for i in range(100):
-    print(f"{i:02d}{', ' if i != 99 else ''}", end="")
-print()
+    if i != 99:
+        print("{:02d}".format(i), end=", ")
+    else:
+        print("{:02d}".format(i))
