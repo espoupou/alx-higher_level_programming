2bc008a3c9e0f861eface2f1bc2522003669690d
espoupou <espoirativon@gmail.com>
2023-06-10 19:03:50 -0700

    12-switch.py

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
-    l = list()
+    table = list()
     for i in my_list:
-        l.append(False if i % 2 else True)
-    return l
+        table.append(False if i % 2 else True)
+    return table
