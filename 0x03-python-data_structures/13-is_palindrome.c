d478a3e2f495984331b60c0771ce4927c960a27e
espoupou <espoirativon@gmail.com>
2023-06-11 19:50:37 -0700

    13-is_palindrome.c

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,20 +1,56 @@
 #include "lists.h"
+#include <stdio.h>
 
 /**
- * getprev - get element before adress in p
- * @head: head of list
- * @p: index pointer
- * Return: the pointer before p
+ * reverse - end to middle orientation
+ * @h: middle head
+ * Return: nothing
  */
 
-listint_t *getprev(listint_t *head, listint_t *p)
+void reverse(listint_t **h)
 {
+	listint_t *p, *q, *cur;
+
+	p = NULL;
+	cur = *h;
+
+	while (cur != NULL)
+	{
+		q = cur->next;
+		cur->next = p;
+		p = cur;
+		cur = q;
+	}
+
+	*h = p;
+}
+
+/**
+ * palin - check if is palin
+ * @h1: first head
+ * @h2: second head
+ * Return: 1 or 0
+ */
+
+int palin(listint_t *h1, listint_t *h2)
+{
+	listint_t *p;
 	listint_t *q;
 
-	for (q = head; q->next != p; q = q->next)
-		;
+	p = h1;
+	q = h2;
 
-	return (q);
+	while (p != NULL && q != NULL)
+	{
+		if (p->n != q->n)
+			return (0);
+		p = p->next;
+		q = q->next;
+	}
+
+	if (p == NULL && q == NULL)
+		return (1);
+	return (0);
 }
 
 /**
@@ -25,26 +61,53 @@ listint_t *getprev(listint_t *head, listint_t *p)
 
 int is_palindrome(listint_t **head)
 {
-	listint_t *p, *q;
+	listint_t *p, *q, *prev, *h2, *middle = NULL;
+	int k = 1;
 
-	if (*head == NULL || (*head)->next == NULL)
-		return (1);
-
-	for (p = *head; p->next != NULL; p = p->next)
-		;
-
+	p = *head;
 	q = *head;
+	prev = *head;
 
-	do {
-		if (p->n != q->n)
-			return (0);
+	if (*head != NULL && (*head)->next != NULL)
+	{
+		while (q != NULL && q->next != NULL)
+		{
+			q = q->next->next;
+			prev = p;
+			p = p->next;
+		}
 
-		p = getprev(*head, p);
-		q = q->next;
-	} while (p != q && p->next != q);
+		if (q != NULL)
+		{
+			middle = p;
+			p = p->next;
+		}
 
-	return (1);
+		h2 = p;
+		prev->next = NULL;
 
+		reverse(&h2);
+		k = palin(*head, h2);
 
+		reverse(&h2);
+
+		if (middle != NULL)
+		{
+			prev->next = middle;
+			prev = prev->next;
+		}
+
+		prev->next = h2;
+		/* if (middle != NULL)
+		{
+			prev_slow->next = middle;
+			middle->next = scn_half;
+		}
+		else
+		{
+			prev_slow->next = scn_half;
+		} */
+	}
+
+	return (k);
 }
-
