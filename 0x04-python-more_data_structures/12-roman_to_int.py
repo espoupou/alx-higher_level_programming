0edc6617fa3d80547076c1de72cd2a60bdf94379
espoupou <espoirativon@gmail.com>
2023-06-14 15:00:45 -0700

    12-roman_to_int.py

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -6,6 +6,8 @@ def roman_to_int(roman_string):
 
     if not roman_string:
         return 0
+    if not isinstance(roman_string, str):
+        return 0
 
     for i in roman_string:
         value = dico.get(i)
