65c0badc28ba5910745c93aafb8a9669bc3103d3
espoupou <126259211+espoupou@users.noreply.github.com>
2023-06-26 23:21:23 +0100

    optional

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -3,7 +3,7 @@ def safe_print_integer_err(value):
     import sys
     try:
         print("{:d}".format(value))
-    except ValueError as err:
+    except Exception as err:
         sys.stderr.write("Exception: {}\n".format(err))
         return False
     else:
