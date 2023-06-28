36534c1e333eee8dfed7052494e440ffa75ce87f
espoupou <espoirativon@gmail.com>
2023-06-27 19:47:27 -0700

    mandatory

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Module that define an empty Square class.
+"""
+
+
+class Square:
+    """class Square that defines a square
+    """
+    def __init__(self, size):
+        """Initialize method that stores the size of the square
+
+        Args:
+            size (int): size of the square
+        """
+        self.__size = size
