ff7b0b4578f69dc0d66bc2ab85bc964a2a5b3876
espoupou <espoirativon@gmail.com>
2023-07-05 05:41:22 -0700

    mandatory

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+
+prevents the user from dynamically creating new inst
+
+"""
+
+
+class LockedClass:
+    __slots__ = ['first_name']
+
+    def __init__(self):
+        """ Init method """
+        pass
---
+++ 0x09-python-everything_is_object/19-copy_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Module of list copy
+"""
+
+def copy_list(l):
+    """copy list function
+    """
+    return l.copy()
