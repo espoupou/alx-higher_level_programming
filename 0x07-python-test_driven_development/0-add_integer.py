ee71d6f5b81f5db7aaef7f6fbc064cc2166c3c45
espoupou <espoirativon@gmail.com>
2023-07-06 13:30:22 -0700

    mandatory

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -2,6 +2,7 @@
 """Module for add function
 """
 
+
 def add_integer(a, b=98):
     """Add function
 
@@ -11,11 +12,14 @@ def add_integer(a, b=98):
 
       Return:
          a + b
+
     """
     if not isinstance(a, int) and not isinstance(a, float):
         raise TypeError("a must be an integer")
     if not isinstance(b, int) and not isinstance(b, float):
         raise TypeError("b must be an integer")
+
     a = int(a)
     b = int(b)
+
     return a + b
